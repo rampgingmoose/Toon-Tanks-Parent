b1b37b5c24d6ef3be30bc98297d9948ee5b603cf
rampgingmoose <76532218+rampgingmoose@users.noreply.github.com>
2023-02-14 15:26:10 -0500

    Initial BasePawn setup for player and enemies

---
+++ ToonTanks/Source/ToonTanks/BasePawn.h
@@ -0,0 +1,59 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "BasePawn.generated.h"
+
+UCLASS()
+class TOONTANKS_API ABasePawn : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	ABasePawn();
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	int32 visibleAnywhereInt = 12;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	int32 EditAnywhere = 22;
+
+	UPROPERTY(VisibleInstanceOnly)
+	int32 visibleInstanceOnlyInt = 10;
+
+	UPROPERTY(EditAnywhere)
+	float tankSpeed = 100.f;
+
+	UPROPERTY(VisibleDefaultsOnly)
+	int32 VisibleDefaultsOnlyInt = 5;
+
+	UPROPERTY(EditDefaultsOnly)
+	int32 EditDefaultsOnlyInt = 9;
+
+	UPROPERTY(EditInstanceOnly)
+	int32 EditInstanceOnlyInt = 14;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
+	UPROPERTY()
+	class UCapsuleComponent* CapsuleComp;
+	UPROPERTY()
+	UStaticMeshComponent* BaseMesh;
+	UPROPERTY()
+	UStaticMeshComponent* TurretMesh;
+	UPROPERTY()
+	USceneComponent* ProjectileSpawnPoint;
+};
